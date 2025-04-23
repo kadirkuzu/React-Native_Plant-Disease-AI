36ecf38e8cf1b3efc6a75ac3483a512bd10bf139
Kadir Kuzu <kadirkuzu.5656@gmail.com>
2025-04-23 04:30:13 +0300

    remove unused

--- plant-disease-app/src/core/helpers/file.helper.ts
+++ plant-disease-app/src/core/helpers/file.helper.ts
@@ -1,15 +1,6 @@
 import * as FileSystem from 'expo-file-system';
 import * as ImageManipulator from 'expo-image-manipulator';
 
-export function base64ToUint8Array(base64: string): Uint8Array {
-  const binary = atob(base64);
-  const array = new Uint8Array(binary.length);
-  for (let i = 0; i < binary.length; i++) {
-    array[i] = binary.charCodeAt(i);
-  }
-  return array;
-}
-
 export const convertToJPEG = async (uri: string) => {
   const manipResult = await ImageManipulator.manipulateAsync(
     uri,
